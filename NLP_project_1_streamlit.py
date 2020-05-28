c4ab3aa9e1070a4025ee46eeebe8ca98a9fec01f
Shashwat2901 <49088356+Shashwat2901@users.noreply.github.com>
2020-05-28 23:45:31 +0530

    Trans

--- NLP_project_1_streamlit.py
+++ NLP_project_1_streamlit.py
@@ -23,10 +23,19 @@ st.markdown('This is a dashboard where you can hover over the country in a map t
 languages = swadesh.fileids()
 
 #iso_alpha codes of countries to use them in form of locations in plotly_express map
-country_codes = ['BLR' , 'BGR' , 'BIH' , 'AND' ,'CZE' , 'MNE','DEU' , 'USA' , 'URY' , 'FRA' , 'HRV' , 'CHE', 'ITA' , 'MKD' , 'ABW', 'POL',
+country_codes = ['BLR' , 'BGR' , 'BIH' , 'AND' ,'CZE' , 'MNE','DEU' , 'USA' , 'ESP' , 'FRA' , 'HRV' , 'CHE', 'ITA' , 'MKD' , 'ABW', 'POL',
  'PRT' , 'ROU' , 'RUS' ,'SVK' , 'SVN' , 'SRB', 'KEN' ,'UKR'  ]
 
+country_names = ['Belarus' , 'Bulgaria' , 'Bosnia and Herzengove' , 'Andorra' , 'Czech Republic', 'Montenegro' ,'Germany','USA' , 'Spain' , 'France' , 'Croatia' ,'Switzerland' ,'Italy' ,'Republic of North Macedonia','Aruba','Poland' ,'Portugal' ,'Romania' , 'Russia' , 'Slovakia' , 'Slovenia', 'Serbia' , 'Kenya' , 'Ukraine']
+
+
+lang2country_name = {languages[i] : country_names[i] for i in range(len(languages))}
+
+
+lang2country = {languages[i] : country_codes[i] for i in range(len(languages))}
+
 #Country_codes converted to pandas dataframe so as to use them in maps
+
 country_codes = pd.DataFrame(country_codes)
 
 #list of words of different languages stored
@@ -34,7 +43,6 @@ x = []
 
 #index of the particular language in the above list 
 indx = dict()
-
 
 i = 0 
 for lang in languages:
@@ -49,8 +57,9 @@ country_codes['lang'] = languages
 
 
 #Check box on sidebar to be unchecked if user wants to see map 
-if  st.sidebar.checkbox('Check to show the Map' ,False):
+if  st.sidebar.checkbox('Check to see country\'s language code' ,False):
 	st.subheader('Hover your mouse over countries to know the language code of various countries')
+	st.markdown('Only blue coloured countries are available for translation')
 	#Map with country_codes being used as locations
 	fig = px.choropleth(country_codes,locations =country_codes[:][0] ,
              projection = 'orthographic' , hover_name = country_codes[:][0],hover_data = ['lang' ])
@@ -61,6 +70,7 @@ if  st.sidebar.checkbox('Check to show the Map' ,False):
 if  st.sidebar.checkbox('Check to show the dictionary options' ,False):
 	#Select Box to select language codes
 	select = st.sidebar.selectbox('Select language to convert English words to' ,(languages), key =1)
+	st.sidebar.markdown('This language is spoken in ' + lang2country[select] + ' i.e ' + lang2country_name[select] )
 	st.subheader('The converted English words of the selected language are following:')
 	k =[]
 	#Printing English words and also translated Words
